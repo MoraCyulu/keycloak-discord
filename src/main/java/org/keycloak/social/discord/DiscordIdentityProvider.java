df108c535de3d0ebf85ef19970c1c24361a92ab7
moracyulu <homupezi01@gmail.com>
2024-07-02 14:42:17 +0900

    [FIX] Identity Context init
    コンストラクタへコンフィグを渡すように修正

--- src/main/java/org/keycloak/social/discord/DiscordIdentityProvider.java
+++ src/main/java/org/keycloak/social/discord/DiscordIdentityProvider.java
@@ -84,7 +84,7 @@ public class DiscordIdentityProvider extends AbstractOAuth2IdentityProvider<Disc
     {
         String id = getJsonProperty(profile, "id");
 
-        BrokeredIdentityContext user = new BrokeredIdentityContext(id);
+        BrokeredIdentityContext user = new BrokeredIdentityContext(id, getConfig());
 
         String username = getJsonProperty(profile, "username");
         String discriminator = getJsonProperty(profile, "discriminator");
@@ -105,7 +105,6 @@ public class DiscordIdentityProvider extends AbstractOAuth2IdentityProvider<Disc
         }
 
         user.setEmail(getJsonProperty(profile, "email"));
-        user.setIdpConfig(getConfig());
         user.setIdp(this);
 
         AbstractJsonUserAttributeMapper.storeUserProfileForMapper(user, profile, getConfig().getAlias());
